f1a570992f23247e5420f6879f26ea37dbb1a46b
Arturo Cabrera <gnuxdar@gmail.com>
2023-03-25 20:13:11 -0500

    [UPD] funciones desde objetos

--- funciones.py
+++ funciones.py
@@ -15,3 +15,11 @@ mifuncionParam(5, 5)
 def suma(num1, num2):
     total = num1 + num2
     return total
+
+# funciones desde objetos
+miTexto = "el perro de san roque no tiene rabo"
+help(str)
+str.title(miTexto)  #poner el principio de cada palabra en mayuscula
+mitexto.title()     # lo mismo que en lo anterior
+mitexto.replace('san','mr')  #replazar palabras
+mitexto.replace('san','mr').title() 
