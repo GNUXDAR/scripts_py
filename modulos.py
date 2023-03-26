94e454cb7ad995fa6679c10da21aa8f01c59c8ed
Arturo Cabrera <gnuxdar@gmail.com>
2023-03-25 20:13:39 -0500

    [NEW] modulos

---
+++ modulos.py
@@ -0,0 +1,20 @@
+# modulos
+# cada uno de los archivos .py que generamos en python se denominan modulos, y son considerados contenedores para organizar nuestro codigo
+
+#paquetes
+# un pauqete es un modulo que sirve para contener otros modulos y paquetes, en la practica es un directorio que contiene un archivo '__init__.py
+# dentro de dicho directorio podremos tener almacenados otros modulos o paquetes
+# .
+# |-- modulo1.py
+# |-- paquete
+#   |-- __init__.py
+#   |-- modulo1.py
+#   |-- subpaquete
+#       |-- __init__.py
+#       |-- modulo1.py
+#       |-- modulo2.py
+
+
+import math
+help(math) # mostrar la descripcion de la libreria
+print(math.cos(math.pi))  #coseno de pi
