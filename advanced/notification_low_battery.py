2d531736a6a110dd62356efd9cc166bcb66f17f4
Arturo Cabrera <gnuxdar@gmail.com>
2023-03-27 23:37:19 -0500

    [UPD] add function to script notification_low_battery

--- advanced/notification_low_battery.py
+++ advanced/notification_low_battery.py
@@ -1,14 +1,16 @@
 import psutil
 import time
 from plyer import notification # asegúrate de tener instalada la biblioteca plyer
+def batery():
+    LOW_BATTERY_THRESHOLD = 20  # porcentaje de batería por debajo del cual se considera que la batería está baja
+    SLEEP_INTERVAL = 5 * 60  # tiempo en segundos entre las verificaciones de la batería, 5 minutos
 
-LOW_BATTERY_THRESHOLD = 35  # porcentaje de batería por debajo del cual se considera que la batería está baja
-SLEEP_INTERVAL = 5 * 60  # tiempo en segundos entre las verificaciones de la batería, 5 minutos
+    while True:
+        battery = psutil.sensors_battery()
+        if battery.percent < LOW_BATTERY_THRESHOLD: # and not battery.power_plugged:
+            notification_title = "X_X Batería Baja X_X"
+            notification_text = f"Nivel de batería: {battery.percent}%"
+            notification.notify(notification_title, notification_text)
+        time.sleep(SLEEP_INTERVAL)
 
-while True:
-    battery = psutil.sensors_battery()
-    if battery.percent < LOW_BATTERY_THRESHOLD: # and not battery.power_plugged:
-        notification_title = "Batería Baja"
-        notification_text = f"Nivel de batería: {battery.percent}%"
-        notification.notify(title=notification_title, message=notification_text)
-    time.sleep(SLEEP_INTERVAL)
+batery()
