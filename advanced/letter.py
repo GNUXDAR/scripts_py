4e2ba5cef85525757cdd21dfa37ab5b8dea47817
Arturo Cabrera <gnuxdar@gmail.com>
2023-04-10 21:45:00 -0500

    [NEW] letter to me

---
+++ advanced/letter.py
@@ -0,0 +1,12 @@
+# today date
+print('+------------------------------------------------------------+')
+print('|                                                2023 04 10  |')
+print('|                                            Ecuador, Quito  |')
+# i am feeling right now
+print('|                                                Amazing     |')
+# i need learn with best practice
+print('|                              i learn programming in python |')
+# message to me older
+print('|                                        Never stop learning |')
+print('|                                            @gnuxdar 🚀     |')
+print('+------------------------------------------------------------+')
