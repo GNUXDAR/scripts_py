b13a43ce6c2aee8b48c8aeed26206965759acaac
Arturo Cabrera <gnuxdar@gmail.com>
2023-04-19 22:29:43 -0500

    [UPD] add comment in mymodule, to import module

--- Introduccion_py/mymodule.py
+++ Introduccion_py/mymodule.py
@@ -1,23 +1,27 @@
 # mymodule.py file
 def generate_full_name(firstname, lastname):
+    """ nombre completo """
     return firstname + ' ' + lastname
 
 
 def sum_two_nums(num1, num2):
+    """ suma dos numeros """
     result = num1 + num2
     return result
 
 
 def person(firstname, lastname, age, rol):
-    person = {
+    """ datos personales """
+    persona = {
         'firstname': firstname,
         'lastname': lastname,
         'age': age,
         'rol': rol
     }
-    return person
+    return persona
 
 
 def gravity():
+    """ valor de la gravedad """
     g = 9.8
     return g
