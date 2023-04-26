7536f2ecb4aebef96f858b2b6ee142755a32e50d
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-04-25 21:45:51 -0500

    [UPD] changed value of variable

--- Introduccion_py/variables.py
+++ Introduccion_py/variables.py
@@ -28,14 +28,14 @@ print('Person information: ', person_info)
 
 
 # Printing out types
-print(type('Asabeneh'))     # str
+print(type('Arturo'))     # str
 print(type(first_name))     # str
 print(type(10))             # int
 print(type(3.14))           # float
 print(type(1 + 1j))         # complex
 print(type(True))           # bool
 print(type([1, 2, 3, 4]))     # list
-print(type({'name':'Asabeneh','age':250, 'is_married':250}))    # dict
+print(type({'name':'Arturo','age':250, 'is_married':250}))    # dict
 print(type((1,2)))                                              # tuple
 print(type(zip([1,2],[3,4])))                                   # set
 
