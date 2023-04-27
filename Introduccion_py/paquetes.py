9305b1e357db536cd98e21e3dfcfaf554d75fb99
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-04-26 20:28:14 -0500

    [NEW] package instructions

---
+++ Introduccion_py/paquetes.py
@@ -0,0 +1,17 @@
+# Crear un paquete - Creating a Package
+# Organizamos una gran cantidad de archivos en diferentes carpetas y subcarpetas basadas en ciertos criterios, 
+# para poder encontrarlos y gestionarlos fácilmente. Como sabes, un módulo puede contener múltiples objetos, 
+# como clases, funciones, etc. Un paquete puede contener uno o más módulos relevantes. 
+# Un paquete es en realidad una carpeta que contiene uno o más archivos de módulo. 
+# Creemos un paquete llamado "mypackage" utilizando los siguientes pasos:
+
+# Crear una nueva carpeta llamada mypackage dentro de la carpeta de este proyecto. 
+# Crear un archivo vacío llamado __init__.py en la carpeta mypackage. Crear los módulos arithmetic.py y greet.py con el siguiente código:
+
+# La estructura de carpetas de su paquete debería verse así:
+─ mypackage
+├── __init__.py
+├── arithmetic.py
+└── greet.py
+
+# Ahora abramos el shell interactivo de python y probemos el paquete que hemos creado:
