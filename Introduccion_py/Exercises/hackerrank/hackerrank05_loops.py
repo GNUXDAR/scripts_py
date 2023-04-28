968dd524c310c0ae8cbda58b09b70cf5ff923889
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-04-28 09:46:10 -0500

    [NEW] solver exercise of hackerrank loops

--- Introduccion_py/Exercises/hackerrank/hackerrank05_loops.py
+++ Introduccion_py/Exercises/hackerrank/hackerrank05_loops.py
@@ -1,11 +1,14 @@
-# users_list = ['Arturo', 'Gnuxdar', 'Admin', 'Siry', 'Alexa', 'GPT']
-# users_to_delete = ['Siry', 'Alexa', 'GPT']
-# new_list = [x for x in users_list if x not in users_to_delete]
-# print(new_list)
+# Task
+# The provided code stub reads and integer,n , from STDIN. For all non-negative integers, i < n print i al cuadrado .
 
+# Example
+# n = 3
+# The list of non-negative integers that are less than is . Print the square of each number on a separate line.
 
-user_list2 = ['user00', 'user01', 'user02', 'user03', 'user04', 'user05']
-to_delete = ['user00', 'user02']
-
-new_list = [item for item in user_list2 if item not in to_delete]
-print(new_list)
+if __name__ == '__main__':
+    n = int(input())
+    if 1 <= n <= 20:
+        for i in range(n):
+            print(i ** 2)
+    else:
+        print('Error!')
