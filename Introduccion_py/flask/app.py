2c8ebcfd895c5e6ee7e3f9fe00cd24732657a849
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-05-06 22:52:34 -0500

    [UPD] renderized the templates

--- Introduccion_py/flask/app.py
+++ Introduccion_py/flask/app.py
@@ -1,6 +1,6 @@
 # Toda aplicación Flask es una instancia WSGI de la clase Flask.
 from flask import Flask, render_template, url_for, request, redirect
-from forms import SignupForm
+from forms import SignupForm, PostForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
@@ -13,7 +13,8 @@ def index():
     page = request.args.get('page', 1)
     list = request.args.get('list', 20)
 
-    return render_template('index.html', title='home', num_post=len(posts))
+    return render_template('index.html', title='home', num_post=len(posts), posts=posts)
+
 
 @app.route('/p/<string:slug>/')
 def show_post(slug):
@@ -22,12 +23,21 @@ def show_post(slug):
 
 
 # crear - editar
-@app.route('/admin/post/')
+@app.route('/admin/post/', methods=['GET', 'POST'])
 @app.route('/admin/post/<int:post_id>/')
 def post_form(post_id=None):
     """ crear y editar """
-    # return f'post_form {post_id}'
-    return render_template('admin/post_form.html', post_id=post_id)
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data
+
+        post = {'title': title, 'title_slug': title_slug, 'content': content}
+        posts.append(post)  #guardando en un dicionario post_form.html
+
+        return redirect(url_for('index'))
+    return render_template('admin/post_form.html', form=form)
 
 # registro
 @app.route('/signup/', methods=['GET', 'POST'])
@@ -38,10 +48,6 @@ def show_signup_form():
         name = form.name.date
         email = form.email.data
         password = form.password.data
-    # if request.method == 'POST':
-    #     name = request.form['name']
-    #     email = request.form['email']
-    #     password = request.form['password']
 
         next = request.args.get('next', None)
         if next:
--- Introduccion_py/flask/forms.py
+++ Introduccion_py/flask/forms.py
@@ -1,10 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, InputRequired
 
-
+# para renderizar correctamente  en las plantillas
 class SignupForm(FlaskForm):
     name = StringField('Nombre', validators=[DataRequired(), Length(max=64)])
-    password = PasswordField('Password', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
     submit = SubmitField('Registrar')
+
+class PostForm(FlaskForm):
+    title = StringField('Título', validators=[DataRequired(), Length(max=128)])
+    title_slug = StringField('Título slug', validators=[Length(max=128)])
+    content = TextAreaField('Contenido')
+    submit = SubmitField('Enviar')
