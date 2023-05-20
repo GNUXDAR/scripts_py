69d2a907da53b4099473d1d23b17877621be3560
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-05-20 17:44:24 -0500

    [UPD] moved

--- Introduccion_py/clases_objetos/cliente.py
+++
@@ -1,6 +0,0 @@
-from persona import Persona
-
-class Cliente(Persona):
-    def __init__(self, nombre, apellido, cedula, telefono, categoria):
-        super().__init__(nombre, apellido, cedula, telefono)
-        self.categoria = categoria
--- Introduccion_py/clases_objetos/empleado.py
+++
@@ -1,6 +0,0 @@
-from persona import Persona
-
-class Empleado(Persona):
-    def __init__(self, nombre, apellido, cedula, telefono, salario):
-        super().__init__(nombre, apellido, cedula, telefono)
-        self.salario = salario
