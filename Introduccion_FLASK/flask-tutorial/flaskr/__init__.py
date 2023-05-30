49d2403cbaddddad3b8896b06960a8e705273977
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-05-30 18:29:42 -0500

    [UPD] add blueprint to  auth

--- Introduccion_FLASK/flask-tutorial/flaskr/__init__.py
+++ Introduccion_FLASK/flask-tutorial/flaskr/__init__.py
@@ -31,5 +31,8 @@ def create_app(test_config=None):
     @app.route('/hello/')
     def hello():
         return 'Hello, World! Arturo'
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
