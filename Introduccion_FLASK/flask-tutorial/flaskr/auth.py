d05adf2254c709db905442d0af50d13be5e5bca7
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-05-30 18:29:10 -0500

    [NEW] file to register users

---
+++ Introduccion_FLASK/flask-tutorial/flaskr/auth.py
@@ -0,0 +1,37 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request-form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is requiered.'
+
+        if error is None:
+            try:
+                db.execute(
+                    " INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f'User {username} is already registered.'
+            else:
+                return redirect(url_for('auth.login'))
+            
+        flash(error)
+    return render_template('auth/register.html')
