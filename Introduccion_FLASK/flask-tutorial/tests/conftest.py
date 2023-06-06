1d1245a02e1c539d1d690930700bab9d0a8edddb
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-06-06 18:08:46 -0500

    [NEW] Unit test files

---
+++ Introduccion_FLASK/flask-tutorial/tests/conftest.py
@@ -0,0 +1,57 @@
+from flaskr.db import get_db, init_db
+from flaskr import create_app
+import pytest
+import tempfile
+import os
+
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True, # indica que sta en modo de prueba
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
