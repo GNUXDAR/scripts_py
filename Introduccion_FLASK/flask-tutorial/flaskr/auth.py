13dcbf3498b21e21da75fffbe8a13d5ce4cea7f7
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-06-07 16:15:52 -0500

    [UPD] fix request.form username

--- Introduccion_FLASK/flask-tutorial/flaskr/auth.py
+++ Introduccion_FLASK/flask-tutorial/flaskr/auth.py
@@ -11,7 +11,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request-form['username']
+        username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
