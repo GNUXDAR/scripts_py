5fc1dc0f2d50aa84932dc46ecdd8450fd35f76b6
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-06-07 17:28:07 -0500

    [UPD] fix

--- Introduccion_FLASK/flask-tutorial/flaskr/__init__.py
+++ Introduccion_FLASK/flask-tutorial/flaskr/__init__.py
@@ -4,41 +4,47 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # crear y configurar la aplicación
+    """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
-    from . import db
-    db.init_app(app)
-
     if test_config is None:
-        # cargar la configuración de la instancia, si existe, cuando no se está realizando pruebas
-        app.config.from_pyfile('config.py', silent=True)
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
     else:
-        # cargar la configuración de prueba si se pasa
-        app.config.from_mapping(test_config)
+        # load the test config if passed in
+        app.config.update(test_config)
 
-    # asegurarse de que la carpeta de instancia exista
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # una página simple que dice hola
-    @app.route('/hello/')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World! Arturo'
-    
-    # blueprint auth
-    from . import auth
-    app.register_blueprint(auth.bp)
+        return "Hello, World!"
 
-    # blueprint blog
-    from . import blog
+    # register the database commands
+    from flaskr import db
+
+    db.init_app(app)
+
+    # apply the blueprints to the app
+    from flaskr import auth, blog
+
+    app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
 
     return app
--- Introduccion_FLASK/flask-tutorial/flaskr/db.py
+++ Introduccion_FLASK/flask-tutorial/flaskr/db.py
@@ -1,14 +1,18 @@
 import sqlite3
 
 import click
-from flask import current_app, g
+from flask import current_app
+from flask import g
 
 
 def get_db():
-    if 'db' not in g:
+    """Connect to the application's configured database. The connection
+    is unique for each request and will be reused if this is called
+    again.
+    """
+    if "db" not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
 
@@ -16,25 +20,33 @@ def get_db():
 
 
 def close_db(e=None):
-    db = g.pop('db', None)
+    """If this request connected to the database, close the
+    connection.
+    """
+    db = g.pop("db", None)
 
     if db is not None:
         db.close()
 
 
 def init_db():
+    """Clear existing data and create new tables."""
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
 
-@click.command('init-db')
+
+@click.command("init-db")
 def init_db_command():
-    """ Borre los datos existentes y cree nuevas tablas. """
+    """Clear existing data and create new tables."""
     init_db()
-    click.echo('Initialized the database.')
+    click.echo("Initialized the database.")
 
 
 def init_app(app):
+    """Register database functions with the Flask app. This is called by
+    the application factory.
+    """
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
